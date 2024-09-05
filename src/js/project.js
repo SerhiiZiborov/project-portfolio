abf0b4d7bbaced60bfdb964e01f87af4ad1a1485
Ziborov Serhii <144032720+Serhii01010101@users.noreply.github.com>
2024-09-06 00:52:51 +0300

    в

--- src/js/project.js
+++ src/js/project.js
@@ -1,4 +1,4 @@
-import { initSwiper } from './swiper';
+// import { initSwiper } from './swiper';
 
 // const projectsInitSwiper = initSwiper('.projects-swiper-container', {
 //   keyboard: {
@@ -13,3 +13,47 @@ import { initSwiper } from './swiper';
 //     prevEl: '.projects-button-prev',
 //   },
 // });
+
+// JavaScript
+// import Swiper from 'swiper';
+// import { Navigation } from 'swiper/modules';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+
+// const swiper = new Swiper('.swiper', {
+//   modules: [Navigation],
+//   navigation: {
+//     nextEl: '.',
+//     prevEl: '.',
+//   },
+// });
+
+// import Swiper from 'swiper';
+// import { Navigation } from 'swiper/modules';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+
+// const swiper = new Swiper('.projects-swiper-container', {
+//   modules: [Navigation], // Указываем модуль навигации
+//   navigation: {
+//     // Настройки навигации
+//     nextEl: '.projects-button-next',
+//     prevEl: '.projects-button-prev',
+//   },
+// });
+
+import Swiper from 'swiper';
+import { Navigation, Keyboard } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const swiper = new Swiper('.projects-swiper-container', {
+  modules: [Navigation, Keyboard],
+  navigation: {
+    nextEl: '.projects-button-next',
+    prevEl: '.projects-button-prev',
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -8,3 +8,22 @@
 // export function initSwiper(containerSelector, options) {
 //   return new Swiper(containerSelector, options);
 // }
+
+// import Swiper from 'swiper';
+
+/*
+	import Swiper from 'swiper';
+	•	Что делает: Импортирует основной класс Swiper из пакета swiper. Этот класс нужен для создания слайдера.
+	•	Откуда: Из пакета Swiper, установленного через NPM.
+*/
+
+// import 'swiper.css';
+
+/*
+
+	•	import 'swiper/css';
+	•	Что делает: Импортирует базовые стили для Swiper. Эти стили нужны для правильного отображения слайдера.
+	•	Откуда: Из пакета Swiper.
+	•	const swiper = new Swiper(...);
+	•	Что делает: Создает новый экземпляр Swiper. Внутри скобок ... должны быть указаны параметры и настройки для вашего слайдера.
+*/
